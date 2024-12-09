230b4516e2dadc4742841ea87316730f3bebde15
sudo-aadhi <sudoaadhi@gmail.com>
2024-12-09 18:59:48 +0530

    Gradient background spread increased

--- app/components/animated/MaximizeCode.tsx
+++ app/components/animated/MaximizeCode.tsx
@@ -11,7 +11,7 @@ const MaximizeCode = () => {
             height: "50px",
             backgroundColor: "#DCA91F",
             position: "absolute",
-            boxShadow: "0 0 800px 50px #6A4CFF",
+            boxShadow: "0 0 800px 55px #6A4CFF",
           }}
         ></div>
         <div className="flex justify-center items-center w-[582px] h-[352px] bg-black rounded-[27px]"></div>
